50a972f54ad2c45e3b24fba365940adee5ef186f
Professional_test <888lin888@gmail.com>
2023-04-02 17:02:04 +0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,68 @@
+from constants import LOG_DIR
+
+
+def mapping(colomn_, data=None):
+    """Возвращает по номеру колонки требуемую колонку из массива"""
+    if data is None:
+        data = data_()
+    colomn_ = int(colomn_)
+
+    return list(map(lambda row: row.split(' ')[colomn_], data))
+
+
+def filter_(str_, data=None):
+    """Возвращает строки по вводимому значению с этим значением"""
+    if data is None:
+        data = data_()
+
+    return list(filter(lambda row: row if str_ in row else None, data))
+
+
+def unique_(data):
+    """Возвращает массив с уникальными значениями"""
+    result = []
+    seen = set()
+    for row in data:
+        if row in seen:
+            continue
+        else:
+            result.append(row)
+            seen.add(row)
+    return result
+
+
+def sorted_(asc, data=None):
+    """Сортирует массив"""
+    if data is None:
+        data = data_()
+    if asc == 'asc':
+        return sorted(data)
+    elif asc == 'desc':
+        return sorted(data, reverse=True)
+
+
+def limited_(value, data=None):
+    """Лимитирует вывод данных с массива"""
+    if data is None:
+        data = data_()
+    value = int(value)
+    counter = 0
+    result = []
+    while counter < value:
+        for i in data:
+            result.append(i)
+            counter += 1
+            if counter == value:
+                break
+
+    return result
+
+
+FILE_NAME = './apache_logs.txt'
+
+
+def data_():
+    """Формирует массив с данными"""
+    with open(LOG_DIR) as f:
+        data = map(lambda v: v.strip(), f)
+        return list(data)
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, abort, request
+from constants import DATA_DIR
+from processing import filter_in_cmd1, map_in_cmd1, sort_in_cmd1, limit_in_cmd1
+
+app = Flask(__name__)
+
+
+@app.route("/perform_query/", methods=["POST"])
+def perform_query():
+    try:
+        cmd1 = request.args.get('cmd1') or request.json.get('cmd1')
+        value1 = request.args.get('value1') or request.json.get('value1')
+        cmd2 = request.args.get('cmd2') or request.json.get('cmd2')
+        value2 = request.args.get('value2') or request.json.get('value2')
+        file_name = request.args.get('file_name') or request.json.get('file_name')
+        log_file = os.path.join(DATA_DIR, file_name)
+        if not os.path.isfile(log_file):
+            abort(400, 'Ошибка. Имя имя файла не несоответствует')
+        elif cmd1 == 'filter':
+            return filter_in_cmd1(cmd1, cmd2, value1, value2)
+        elif cmd1 == 'map':
+            return map_in_cmd1(cmd1, cmd2, value1, value2)
+        elif cmd1 == 'sort':
+            return sort_in_cmd1(cmd1, cmd2, value1, value2)
+        elif cmd1 == 'limit':
+            return limit_in_cmd1(cmd1, cmd2, value1, value2)
+        else:
+            abort(400, 'Введенные данные недопустимы')
+    except Exception as e:
+        abort(400, e)
